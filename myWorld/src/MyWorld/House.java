acfd91fbad39da68c164fed5f63837c90515676b
totó <ricardo_cerqueira1@hotmail.com>
2024-09-27 19:14:41 +0100

    outras merdas

--- myWorld/src/MyWorld/House.java
+++ myWorld/src/MyWorld/House.java
@@ -1,2 +1,78 @@
-package MyWorld;public class House {
+package MyWorld;
+
+public class House {
+    private String colour;
+    private String type;
+    private String typeConstruction;
+    private int value;
+    public int proposal;
+
+    public House(String someColour, String someType, String someTypeConstruction) {
+        this.colour = someColour;
+        this.type = someType;
+        this.typeConstruction = someTypeConstruction;
+
+    }
+
+    public String getColour() {
+        return this.colour;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public String getTypeConstruction() {
+        return this.typeConstruction;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+
+
+    public void changeColour(String newColour) {
+        if (newColour.equals("blue") || newColour.equals("green") || newColour.equals("white") || newColour.equals("black")) {
+            this.colour = newColour;
+        } System.out.println("Cor não aceite");
+    }
+
+    public void changeType(String someType) {
+        if (someType.equals("T0") || someType.equals("T1") || someType.equals("T2")) {
+            this.type = someType;
+        }
+        System.out.println("Tipologia não aceite");
+    }
+
+    public void changeTypeConstruction(String newTypeConstruction) {
+        if (newTypeConstruction.equals("New") || newTypeConstruction.equals("Old")) {
+            this.typeConstruction = newTypeConstruction;
+        } 
+    }
+
+    public void value() {
+        if (type.equals("T0") && typeConstruction.equals("New")) {
+            value = 100000;
+        } else if (type.equals("T0") && typeConstruction.equals("Old")) {
+            value = 60000;
+        } else if (type.equals("T1") && typeConstruction.equals("New")) {
+            value = 200000;
+        } else if (type.equals("T1") && typeConstruction.equals("Old")) {
+            value = 150000;
+        } else if (type.equals("T2") && typeConstruction.equals("New")) {
+            value = 300000;
+        } else if (type.equals("T2") && typeConstruction.equals("Old")) {
+            value = 250000;
+        } else {
+            System.out.println("Value or type undefined");
+        }
+    }
+
+    public void sell() {
+        if (value < proposal) {
+            System.out.println("Good offer, sell");
+        } else {
+            System.out.println("Bad offer, don't sell");
+        }
+    }
 }
--- myWorld/src/MyWorld/MyWorld.java
+++ myWorld/src/MyWorld/MyWorld.java
@@ -1,2 +1,31 @@
-package MyWorld;public class MyWorld {
+package MyWorld;
+
+
+public class MyWorld {
+
+    public static void main(String[] args) {
+
+        House house1 = new House("white", "T1", "New");
+        house1.value();
+        System.out.println(house1.getColour());
+        System.out.println(house1.getType());
+        System.out.println(house1.getTypeConstruction());
+        System.out.println(house1.getValue());
+        house1.proposal = 190000;
+        house1.sell();
+
+        House house2 = new House("blue", "T2", "Old");
+        house2.value();
+        System.out.println(house2.getColour());
+        System.out.println(house2.getType());
+        System.out.println(house2.getTypeConstruction());
+        System.out.println(house2.getValue());
+        house2.proposal = 260000;
+        house2.sell();
+
+
+
+    }
+
+
 }
