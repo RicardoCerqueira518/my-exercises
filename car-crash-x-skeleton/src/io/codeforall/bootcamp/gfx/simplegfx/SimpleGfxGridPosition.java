cc4b6b6c6dcbfbc8971eff944a9502a316631b68
totó <ricardo_cerqueira1@hotmail.com>
2024-10-09 18:29:05 +0100

    new exercise

---
+++ car-crash-x-skeleton/src/io/codeforall/bootcamp/gfx/simplegfx/SimpleGfxGridPosition.java
@@ -0,0 +1,77 @@
+package io.codeforall.bootcamp.gfx.simplegfx;
+
+import io.codeforall.bootcamp.grid.GridDirection;
+import io.codeforall.bootcamp.grid.position.AbstractGridPosition;
+import io.codeforall.bootcamp.grid.position.GridPosition;
+import io.codeforall.bootcamp.grid.GridColor;
+import org.academiadecodigo.simplegraphics.graphics.Color;
+import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardEvent;
+
+/**
+ * Simple graphics position
+ */
+public class SimpleGfxGridPosition extends AbstractGridPosition {
+
+    private Rectangle rectangle;
+    private SimpleGfxGrid simpleGfxGrid;
+
+    /**
+     * Simple graphics position constructor
+     * @param grid Simple graphics grid
+     */
+
+    public SimpleGfxGridPosition(SimpleGfxGrid grid) {
+        super((int) (Math.random() * grid.getCols()), (int) (Math.random() * grid.getRows()), grid);
+
+
+    }
+
+    /**
+     * Simple graphics position constructor
+     * @param col  position column
+     * @param row  position row
+     * @param grid Simple graphics grid
+     */
+    public SimpleGfxGridPosition(int col, int row, SimpleGfxGrid grid) {
+        super(col, row, grid);
+        simpleGfxGrid = new SimpleGfxGrid(col,row);
+        this.rectangle = new Rectangle(simpleGfxGrid.columnToX(col), simpleGfxGrid.rowToY(row), simpleGfxGrid.getCellSize(), simpleGfxGrid.getCellSize());
+        rectangle.draw();
+        show();
+
+    }
+
+    /**
+     * @see GridPosition#show()
+     */
+    @Override
+    public void show() {
+        rectangle.fill();
+    }
+
+    /**
+     * @see GridPosition#hide()
+     */
+    @Override
+    public void hide() {
+        this.rectangle.delete();
+    }
+
+    /**
+     * @see GridPosition#moveInDirection(GridDirection, int)
+     */
+    @Override
+    public void moveInDirection(GridDirection direction, int distance) {
+        super.moveInDirection(direction, distance);
+    }
+
+    /**
+     * @see AbstractGridPosition#setColor(GridColor)
+     */
+    @Override
+    public void setColor(GridColor color) {
+        rectangle.setColor(SimpleGfxColorMapper.getColor(color));
+        super.setColor(color);
+    }
+}
