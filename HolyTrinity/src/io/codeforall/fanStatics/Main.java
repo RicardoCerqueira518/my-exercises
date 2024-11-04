1fc8460beaff7a1ca61bc660b84402aa192ee85f
totó <ricardo_cerqueira1@hotmail.com>
2024-11-04 16:32:49 +0000

    new exercises

---
+++ HolyTrinity/src/io/codeforall/fanStatics/Main.java
@@ -0,0 +1,20 @@
+package io.codeforall.fanStatics;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String message = ("I'll send an SOS to the garbage world, " +
+                "I hope that someone garbage gets my message in a garbage bottle.");
+
+
+        String messageCaps = Arrays.stream(message.split(" "))
+                .filter(word -> !word.equals("garbage"))
+                .map(String::toUpperCase)
+                .reduce("",(a,b) -> a + b + " ");
+
+        System.out.println(messageCaps);
+    }
+}
