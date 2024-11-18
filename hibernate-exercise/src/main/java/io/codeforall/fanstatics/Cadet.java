9fe1ae26dac1d4f6b8119dace685db06023b90bc
totó <ricardo_cerqueira1@hotmail.com>
2024-11-18 17:25:42 +0000

    add exercise Hibernate

---
+++ hibernate-exercise/src/main/java/io/codeforall/fanstatics/Cadet.java
@@ -0,0 +1,62 @@
+package io.codeforall.fanstatics;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Cadet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Version
+    private Integer version;
+
+    @CreationTimestamp
+    private Date creationTime;
+
+    @UpdateTimestamp
+    private Date updateTime;
+
+
+    private String name;
+    private String email;
+    private Integer age;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
